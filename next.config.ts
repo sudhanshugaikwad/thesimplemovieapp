0edaa4ffe8cdd45eee71b4a0dfb8eb402e10195d
Sudhanshu Gaikwad <sudhanshugaikwad517@gmail.com>
2025-07-16 15:58:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
